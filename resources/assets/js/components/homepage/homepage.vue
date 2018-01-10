5e3a9e43c2979990e86855c4ac5e822835826708
XueCong <xuecong98@gmail.com>
2018-01-10 08:27:27 +0000

    Increase particles background

--- resources/assets/js/components/homepage/homepage.vue
+++ resources/assets/js/components/homepage/homepage.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="root">
-    <div>路由</div>
+  <div class="homepage">
+    <div id="particles"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  import particles from 'particles.js'
+
+  export default {
+    mounted() {
+      particlesJS.load('particles', '/particles/particles.json')
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  .homepage
+    position: relative
+    height: 100vh
+    overflow: hidden
+    #particles
+      position: absolute
+      width: 100%
+      height: 100%
 </style>
