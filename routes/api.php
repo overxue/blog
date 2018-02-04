070ce76aebdb0a58f1eeb9892d4aa3b054d5083c
XueCong <xuecong98@gmail.com>
2018-02-04 15:02:13 +0000

    category rate limit

--- routes/api.php
+++ routes/api.php
@@ -31,19 +31,28 @@ $api->version('v1', [
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
+    });
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function ($api) {
         // 游客可以访问的接口
         // 博文分类列表
         $api->get('categories', 'CategoriesController@index')
             ->name('api.categories.index');
+
         // 需要 token 验证的接口
-        // 添加博文分类
-        $api->post('categories', 'CategoriesController@store')
-            ->name('api.categories.store');
-        // 修改博文分类
-        $api->patch('categories/{category}', 'CategoriesController@update')
-            ->name('api.categories.update');
-        // 删除博文分类
-        $api->delete('categories/{category}', 'CategoriesController@destory')
-            ->name('api.categories.destory');
-        });
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 添加博文分类
+            $api->post('categories', 'CategoriesController@store')
+                ->name('api.categories.store');
+            // 修改博文分类
+            $api->patch('categories/{category}', 'CategoriesController@update')
+                ->name('api.categories.update');
+            // 删除博文分类
+            $api->delete('categories/{category}', 'CategoriesController@destory')
+                ->name('api.categories.destory');
+            });
+    });
 });
